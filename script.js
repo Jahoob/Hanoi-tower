0b0bd9fe3052822b66b4a53f43e77de012b77b9f
Jan Bartosiewicz <jan.bartosiewicz@veneo.eu>
2022-02-25 16:55:46 +0100

    added script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function allowDrop(ev) {
     ev.preventDefault();
+    console.log("JANKO1");
 }
 
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
+    console.log("JAN2");
 }
 
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
+    console.log("JAN3");
   }
